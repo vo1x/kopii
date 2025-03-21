ff1a3841ec360340c6250ccdaba5a0227fef8880
vo1x <ak2061@icloud.com>
2025-03-21 19:57:05 +0545

    ⌨️ fix: removed unused file and updated ui

--- src/renderer/screens/main.tsx
+++ src/renderer/screens/main.tsx
@@ -50,11 +50,12 @@ export function MainScreen() {
   return (
     <div className="flex-col flex h-full gap-2 p-4 ">
       <input
+        disabled={history?.length===0}
         value={searchTerm}
         onChange={e => setSearchTerm(e.target.value)}
         placeholder="Search clipboard history..."
         type="text"
-        className="border-gray-700 focus:border-teal-600 border placeholder:text-gray-500 bg-gray-900 outline-none w-full rounded-md p-2 mb-2 text-gray-200"
+        className="border-gray-700 disabled:opacity-50 disabled:cursor-not-allowed focus:border-teal-600 border placeholder:text-gray-500 bg-gray-900 outline-none w-full rounded-md p-2 mb-2 text-gray-200"
       />
       {filteredHistory.map((item: any) => (
         <ClipboardItem
